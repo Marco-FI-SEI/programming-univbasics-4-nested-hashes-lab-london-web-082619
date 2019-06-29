868c328c8b0fa3ffd1999bdf4a67f77832a98892
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-29 11:02:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,6 +39,8 @@ def alan_kay_is_known_for
      }
      
      programmer_hash[:alan_kay][:known_for]
+     programmer_hash
+     
 end
 
 def dennis_ritchies_language
@@ -83,6 +85,7 @@ def adding_matz
      }
 
      programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
+     programmer_hash
 end
 
 def changing_alan
@@ -105,6 +108,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
